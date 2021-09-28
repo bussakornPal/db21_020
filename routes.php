a850b02ca1f999a4a9528a7a8a35b60aa19c16d1
samthanakorn <91516707+samthanakorn@users.noreply.github.com>
2021-09-28 22:33:48 +0700

    create routes

---
+++ routes.php
@@ -0,0 +1,22 @@
+<?php
+$controllers = array('pages'=>['home','error']);
+function call($controllers,$action){
+    echo "routes to".$controller."-".$action."<br>";
+}
+if(array_key_exits($controllers,$controllers))
+{
+    
+if(in_array($action,$controllers[$controller]))
+{​​​​
+    call($controller,$action);
+}​​​​
+else
+{​​​​
+    call('pages','error');
+}​​​​
+}
+else 
+{
+    call('pages','error')
+}
+?>
