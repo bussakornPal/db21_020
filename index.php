501ca681d8061a7f9fe48afbc6c185e099df3db1
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2021-09-28 12:51:41 +0700

    add

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-hello gym
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+
+</body>
+</html>
