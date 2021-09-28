623d2c4906fb607929546b1e36d4fc61e58d7f53
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2021-09-28 22:05:52 +0700

    a

--- connect_server.php
+++ connect_server.php
@@ -1,13 +1,13 @@
 <?php
     $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname = "databaseName";
+    $username = "db21_020";
+    $password = "db21_020";
+    $dbname = "db21_020";
 
     $conn = new mysqli($servername,$username,$password);
 
     if($conn->connect_error){
-        die("Connection failed:".$coo->connect_error);
+        die("Connection failed:".$conn->connect_error);
     }
     echo "Successfully connected to server <br>";
     $conn->close();
