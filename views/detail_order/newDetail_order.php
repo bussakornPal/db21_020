c07a3e250a0142c305e3a345523670690cb22944
bussakornPal <bussakorn.pal@ku.th>
2021-09-29 23:49:41 +0700

    update view newDetail

--- views/detail_order/newDetail_order.php
+++ views/detail_order/newDetail_order.php
@@ -3,7 +3,10 @@
 <label> จำนวนสีสกรีน <input type="text" name="print_color"/> </label><br>
 <label> จำนวนที่สั่ง <input type="text" name="print_color"/> </label><br>
 <label> รหัสออเดอร์ <select name="id_order_cus">
-    <?php foreach($order_List as $order){echo "<option value=$order->id_order"}
-</select>
+    <?php foreach($order_List as $order){echo "<option value=$order->id_order";}?>
+</select> </label><br>
+<input type="hidden" name="controller" value="detail_order"/>
+<button type="submit"name="action" value="index"> Back </button>
+<button type="submit"name="action" value="newDetail_order"> Save </button>
 
 </from>
