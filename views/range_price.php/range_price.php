20566d698ec9503b1f93fe3b8cb1c955880a20f2
samthanakorn <91516707+samthanakorn@users.noreply.github.com>
2021-09-29 21:44:44 +0700

    crete range_price view

---
+++ views/range_price.php/range_price.php
@@ -0,0 +1,10 @@
+<table border=1>
+<tr><td>id_range</td><td>min_range</td><td>max_range</td><td>price</td><td>color_price</td>
+<td>id_product</td></tr>
+<?php foreach($range_price_List as $range_price)
+{
+        echo "<tr><td>$range_price->id_range</td><td>$range_price->min_range</td><td>$range_price->max_range</td><td>$range_price->price</td>
+        <td>$range_price->color_price</td><td>$range_price->id_product</td><td>update</td><td>delete</td></tr>";
+}
+echo "</table>";
+?>
