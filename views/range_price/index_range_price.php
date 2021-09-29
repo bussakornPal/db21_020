56cbc49b4e8acd57db888e5c4bd94dcbe5a05cf5
samthanakorn <91516707+samthanakorn@users.noreply.github.com>
2021-09-29 22:31:33 +0700

    edit

--- views/range_price/index_range_price.php
+++ views/range_price/index_range_price.php
@@ -1,6 +1,6 @@
 <table border=1>
 <tr><td>id_range</td><td>min_range</td><td>max_range</td><td>price</td><td>color_price</td>
-<td>id_product</td></tr>
+<td>id_product</td></tr><td>update</td><td>delete</td></tr>
 <?php foreach($range_price_List as $range_price)
 {
         echo "<tr><td>$range_price->id_range</td><td>$range_price->min_range</td><td>$range_price->max_range</td><td>$range_price->price</td>
