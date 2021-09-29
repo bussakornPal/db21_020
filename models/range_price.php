a5c9ca999640533825aaf413d550eafe07bc6ea9
samthanakorn <91516707+samthanakorn@users.noreply.github.com>
2021-09-29 22:02:39 +0700

    edit

--- models/range_price.php
+++ models/range_price.php
@@ -5,21 +5,21 @@ public function __construct($id_range,$min_range,$max_range,$price,$color_price,
     $this->min_range = $min_range;
     $this->max_range = $max_range;
     $this->price = $price;
-    $this->$color_price = $color_price;
-    $this->$id_product = $id_product; 
+    $this->color_price = $color_price;
+    $this->id_product = $id_product; 
 }
 public static function getAll(){
     $range_priceList = [];
     require("connection_database.php");
     $sql = " select * from range_price ";
     $result = $conn->query($sql);
-    while($my_row == $result->fetch_assoc()){
-        $ranid_range = $my_row[ranid_range];
-        $ranmin_range = $my_row[ranmin_range];
-        $ranmax_range = $my_row[ranmax_range];
-        $ranprice = $my_row[ranprice];
-        $rancolor_price = $my_row[rancolor_price];
-        $ranid_product = $my_row[ranid_product];
+    while($my_row = $result->fetch_assoc()){
+        $id_range = $my_row[id_range];
+        $min_range = $my_row[min_range];
+        $max_range = $my_row[max_range];
+        $price = $my_row[price];
+        $color_price = $my_row[color_price];
+        $id_product = $my_row[id_product];
         $range_priceList = new range_price($ranid_range,$ranmin_range,$ranmax_range,$ranprice,$rancolor_price,$ranid_product);
     }
     require("connection_close.php");
