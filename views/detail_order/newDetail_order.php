8c7f8877d4255c5c6c5a7f5426ed6d1b8759ee32
bussakornPal <bussakorn.pal@ku.th>
2021-09-30 00:14:38 +0700

    update

--- views/detail_order/newDetail_order.php
+++ views/detail_order/newDetail_order.php
@@ -3,7 +3,7 @@
 <label> จำนวนสีสกรีน <input type="text" name="print_color"/> </label><br>
 <label> จำนวนที่สั่ง <input type="text" name="amount_order_cus"/> </label><br>
 <label> รหัสออเดอร์ <select name="id_order_cus">
-    <?php foreach($order_List as $order){echo "<option value=$order->id_order_cus";}?>
+    <?php foreach($order_List as $order){echo "<option value=$order->id_order_cus> $order->date_order</option>";}?>
 </select> </label><br>
 <input type="hidden" name="controller" value="detail_order"/>
 <button type="submit"name="action" value="index"> Back </button>
