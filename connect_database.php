b5bd172be4a6c63f716fdc21fb82bfe2e526d47d
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2021-09-29 11:11:34 +0700

    Update connect_database.php

--- connect_database.php
+++ connect_database.php
@@ -5,12 +5,18 @@
     $dbname = "db21_020";
 
     $conn = new mysqli($servername,$username,$password);
+    if($conn->connect_error){
+        die("Connection failed:".$conn->connect_error);
+    }
+    echo "Successfully connected to server <br>";
+
+
     if(!$conn->select_db($dbname)){
         echo $conn->connect_error;
     }else{
         echo "Successfully connected to database<br>";
     }
-    $conn->close();
+    
 
 
 ?>
