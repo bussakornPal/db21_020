fd6fb95e95fd1ad0eed28fdb564cf55033e0eabb
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2021-09-29 20:09:08 +0700

    edit

--- index.php
+++ index.php
@@ -14,9 +14,9 @@ if(isset($_GET['controller'])&&isset($_GET['action']))
         <?php echo "controller= ".$controller.",action=".$action;?>
         <br>[<a href="?controller=pages&action=home">home</a>]
         [<a href="?controller=order&action=index">order</a>]
-        <!-- [<a href="?controller=detail_order_customer&action=index">detail_order_customer</a>]
+      [<a href="?controller=detail_order_customer&action=index">detail_order_customer</a>]
         [<a href="?controller=range_price&action=index">range_price</a>]<br>
-         -->
+         
         <?php require_once("./routes.php");?>
     </body>
 </html>
