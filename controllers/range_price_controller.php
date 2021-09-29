b563d05a8f025e9fd2e0eadc180e31766b65c89d
samthanakorn <91516707+samthanakorn@users.noreply.github.com>
2021-09-29 21:07:14 +0700

    crete rangeprice_controller

---
+++ controllers/range_price_controller.php
@@ -0,0 +1,9 @@
+<?php class Range_PriceController{
+    public function index()
+    {
+        $range_price_List=range_price::getAll();
+        require_once("./views/range_price/index_order.php");
+    }
+
+}
+?>
