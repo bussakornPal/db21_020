0c51ca4fa553cb8abd1b1845fa1c1447c567f925
bussakornPal <bussakorn.pal@ku.th>
2021-09-29 11:21:34 +0700

    update route

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
 $controllers = array('pages'=>['home','error']);
-function call($controllers,$action){
+function call($controller,$action){
     require_once("controllers/".$controller."_controller.php");
     switch($controller)
     {
