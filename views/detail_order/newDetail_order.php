38a787d35c2785a44157d14d9237a47119f356de
bussakornPal <bussakorn.pal@ku.th>
2021-09-30 00:00:32 +0700

    update view newdetail

--- views/detail_order/newDetail_order.php
+++ views/detail_order/newDetail_order.php
@@ -1,12 +1,12 @@
-<from method="get" action="">
+<form method="get" action="">
 <label> รหัสรายละเอียด <input type="text" name="id_doc"/> </label><br>
 <label> จำนวนสีสกรีน <input type="text" name="print_color"/> </label><br>
-<label> จำนวนที่สั่ง <input type="text" name="print_color"/> </label><br>
+<label> จำนวนที่สั่ง <input type="text" name="amount_order_cus"/> </label><br>
 <label> รหัสออเดอร์ <select name="id_order_cus">
-    <?php foreach($order_List as $order){echo "<option value=$order->id_order";}?>
+    <?php foreach($order_List as $order){echo "<option value=$order->id_order_cus";}?>
 </select> </label><br>
 <input type="hidden" name="controller" value="detail_order"/>
 <button type="submit"name="action" value="index"> Back </button>
-<button type="submit"name="action" value="newDetail_order"> Save </button>
+<button type="submit"name="action" value="addDetail_order"> Save </button>
 
-</from>
+</form>
