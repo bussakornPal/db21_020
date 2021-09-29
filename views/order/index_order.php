ba8274a37d1c4559005b9edb7d0b1b963b6b282a
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2021-09-30 02:30:04 +0700

    dd

--- views/order/index_order.php
+++ views/order/index_order.php
@@ -1,12 +1,11 @@
 <table border=1>
 new order <a href=?controller=student&action=newOrder> click</a><br>
-<tr><td>id_order_cus</td><td>date_order</td><td>condition_payment</td><td>status_deli</td><td>status_produce</td>
-<td>date_payment_1</td><td>date_payment_2</td><td>date_produce</td><td>id_staff</td><td>id_customer</td><td>update</td><td>delete</td></tr>
+<tr><td>id_order_cus</td><td>date_order</td><td>id_staff</td><td>fname_staff</td><td>id_customer</td><td>name_customer</td><td>update</td><td>delete</td></tr>
 <?php foreach($order_List as $order)
 {
-        echo "<tr><td>$order->id_order_cus</td><td>$order->date_order</td><td>$order->condition_payment</td><td>$order->status_deli</td>
-        <td>$order->status_produce</td><td>$order->date_payment_1</td><td>$order->date_payment_2</td><td>$order->date_produce</td><td>$order->id_staff</td>
-        <td>$order->id_customer</td><td>update</td><td>delete</td></tr>";
+        echo "<tr><td>$order->id_order_cus</td><td>$order->date_order</td>
+        <td>$order->id_staff</td><td>$order->fname_staff</td>
+        <td>$order->id_customer</td><td>$order->name_customer</td><td>update</td><td>delete</td></tr>";
 }
 echo "</table>";
 ?>
