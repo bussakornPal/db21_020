6d12fe6463aeaa3259ed24db0a5d50995087e024
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2021-09-29 23:34:06 +0700

    add

--- controllers/order_controller.php
+++ controllers/order_controller.php
@@ -4,6 +4,11 @@
         $order_List=Order::getAll();
         require_once("./views/order/index_order.php");
     }
+    public function newOrder(){
+        $staff_List=staff::getAll();
+        $customer_List=customer::getAll();
+        require_once('./views/order/newOrder.php');
+    }
 
 }
 ?>
---
+++ views/order/newOrder.php
@@ -0,0 +1,17 @@
+<from method="get" action="">
+<label>รหัส <input type="text" name="id_order_cus"/></label><br>
+<label>วันที่ <input type="text" name="date_order"/></label><br>
+<label>ชื่อลูกค้า <select name="id_customer">
+    <?php foreach($customer_List as $dep) {echo "<option value = $dep->id_customer>
+    $dep->name_customer</option>";}
+    ?>
+</label><br>
+<label>ชื่อพนักงาน <select name="id_staff">
+    <?php foreach($staff_List as $dep) {echo "<option value = $dep->id_staff>
+    $dep->fname_staff</option>";}
+    ?>
+</label><br>
+<input type="hidden"name="controller"value="student"/>
+<botton type= "submit"name="action"value="index"></botton>
+<botton type= "submit"name="action"value="addOrder">Save</botton>
+</from>
