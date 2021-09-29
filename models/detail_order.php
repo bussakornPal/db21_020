5a651b8d9b23db67bb2cc798164c28408918f04e
bussakornPal <bussakorn.pal@ku.th>
2021-09-29 12:24:15 +0700

    update model

--- models/detail_order.php
+++ models/detail_order.php
@@ -1,6 +1,6 @@
 <?php class order_detail{
     public $id_doc,$print_color,$amount_order_cus,$id_order_cus,$id_cop;
-    public function_construct($id_doc,$print_color,$amount_order_cus,$id_order_cus,$id_cop)
+    public order_detail($id_doc,$print_color,$amount_order_cus,$id_order_cus,$id_cop)
     {
         $this->id_doc = $id_doc;
         $this->print_color = $print_color;
