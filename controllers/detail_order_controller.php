83b2d6c7281c6d21171c771326b0c1c4b317d35e
bussakornPal <bussakorn.pal@ku.th>
2021-10-02 00:28:26 +0700

    t2

--- controllers/detail_order_controller.php
+++ controllers/detail_order_controller.php
@@ -7,10 +7,10 @@
     }
     public function newdetail_order()
     {
-        echo "hi";
+        //echo "hi";
         $order_List = Order::getAll();
         $color_of_product_List = color_of_product::getAll();
-        require_once("./view/detail_order/newDetail_order.php");
+        require_once('./view/detail_order/newDetail_order.php');
     }
     public function search()
     {
