0a762234c5a9e87054334b38c28fedf1348cf64e
ThidaratPi <91516629+ThidaratPi@users.noreply.github.com>
2021-10-01 20:54:01 +0700

    e

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php
-$controllers = array('pages'=>['home','error'],'order'=>['index','newOrder','addOrder','updateForm','update','deleteConfirm'],'detail_order'=>['index','newdetail_order'],'range_price'=>['index','newrange_price']);
+$controllers = array('pages'=>['home','error'],'order'=>['index','newOrder','addOrder','search','updateForm','update','deleteConfirm'],'detail_order'=>['index','newdetail_order'],'range_price'=>['index','newrange_price']);
 
 function call($controller,$action){
     require_once("./controllers/".$controller."_controller.php");
--- views/order/index_order.php
+++ views/order/index_order.php
@@ -1,6 +1,11 @@
 <table border=1>
 new order <a href=?controller=order&action=newOrder> click</a><br>
-
+<form method="get" action="">
+        <input type="text" name="key">
+        <input type="hidden" name="controller" value="order">
+        <button type="submit" name="action" value="search">
+search</button>
+</form>
 <tr><td>id_order_cus</td><td>date_order</td><td>fname_staff</td><td>name_customer</td><td>address_customer</td><td>phone</td><td>update</td><td>delete</td></tr>
 <?php foreach($order_List as $order)
 {
