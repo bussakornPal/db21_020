d4591c88b1058dec1a2d6b70a3d341377668e982
bussakornPal <bussakorn.pal@ku.th>
2021-10-02 22:30:58 +0700

    click update

--- views/detail_order/index_detail_order.php
+++ views/detail_order/index_detail_order.php
@@ -11,7 +11,16 @@ search</button>
 <tr> <td>id_doc</td> <td>id_order_cus</td> <td>id_cop</td><td>name_product</td> <td>name_color</td> <td>print_color</td> <td>amount_order_cus</td>  <td>update</td> <td>delete</td> </tr> 
 <?php foreach($detail_order_List as $detail_order)
 {
-    echo "<tr> <td>$detail_order->id_doc</td> <td>$detail_order->id_order_cus</td> <td>$detail_order->id_cop</td> <td>$detail_order->name_product</td> <td>$detail_order->name_color</td>  <td>$detail_order->print_color</td> <td>$detail_order->amount_order_cus</td> <td>update</td> <td>delete</td> </tr>";
+    echo "<tr> <td>$detail_order->id_doc</td> 
+    <td>$detail_order->id_order_cus</td> 
+    <td>$detail_order->id_cop</td> 
+    <td>$detail_order->name_product</td> 
+    <td>$detail_order->name_color</td>  
+    <td>$detail_order->print_color</td> 
+    <td>$detail_order->amount_order_cus</td> 
+    <td><a href=?controller=detail_order&action=updateForm&id_doc=$detail_order->id_doc>update</a></td> 
+    <td>delete</td> 
+    </tr>";
 }
 echo "</table>";
 ?>
