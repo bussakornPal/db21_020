e14a652ca68f4b2914198739e786038a8c3ffcc7
bussakornPal <bussakorn.pal@ku.th>
2021-10-02 23:27:52 +0700

    update views upadteForm

--- views/detail_order/newDetail_order.php
+++ views/detail_order/newDetail_order.php
@@ -4,7 +4,7 @@
 <label> จำนวนสีสกรีน <input type="number" name="print_color"/> </label><br>
 <label> จำนวนที่สั่ง <input type="number" name="amount_order_cus"/> </label><br>
 <label> รหัสออเดอร์ <select name="id_order_cus">
-    <?php foreach($order_List as $order){echo "<option value = $order->id_order_cus> $order->id_order_cus</option>";}?>
+    <?php foreach($order_List as $ord){echo "<option value = $ord->id_order_cus> $ord->id_order_cus</option>";}?>
 </select> </label><br>
 
 <label>สินค้า <select name="id_cop">
--- views/detail_order/updateForm.php
+++ views/detail_order/updateForm.php
@@ -2,5 +2,25 @@
 
 <label> รหัสรายละเอียด <input type="text" name="id_doc"
     value="<?php echo $detail_order->id_doc;?>"/> </label><br>
+<label> จำนวนสีสกรีน <input type="number" name="print_color" 
+    value="<?php echo $detail_order->print_color;?>"/> </label><br>
+<label> จำนวนที่สั่ง <input type="number" name="amount_order_cus"
+    value="<?php echo $detail_order->amount_order_cus;?>"/> </label><br>
+
+<label> รหัสออเดอร์ <select name="id_order_cus">
+    <?php foreach($order_List as $ord)
+        {echo "<option value = $ord->id_order_cus";
+        if($ord->id_order_cus==$order->id_order_cus){echo " selected='selected'";}
+        echo "> $ord->id_order_cus</option>";}
+    ?>
+    </select> </label><br>
+
+<label>สินค้า <select name="id_cop">
+    <?php foreach($color_of_product_List as $cop) 
+        {echo "<option value = $cop->id_cop";
+        if($cop->id_cop==$color_of_product->id_cop){echo " selected='selected'";}
+        echo "> $cop->name_product $cop->name_color</option>";}
+        ?>
+    </select></label><br>
 
 </form>
