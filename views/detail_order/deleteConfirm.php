5a53e4b40909044ecd32e450c05aff3f53039002
bussakornPal <bussakorn.pal@ku.th>
2021-10-03 13:57:11 +0700

    update2

--- views/detail_order/deleteConfirm.php
+++ views/detail_order/deleteConfirm.php
@@ -1,7 +1,7 @@
 <?php echo "<br>Are you sure to delete this detail order<br>";
     echo "รหัสรายละเอียด : $detail_order->id_doc <br>";
     echo "รหัสออร์เดอร์ : $detail_order->id_order_cus<br>";
-    echo "สินค้า : $detail_order->name_product<br>";
+    echo "สินค้า : $detail_order->name_product $detail_order->name_color<br>";
     echo "จำนวนสีสกรีน : $detail_order->print_color<br>";
     echo "จำนวนที่สั่ง : $detail_order->amount_order_cus<br>";
     
