760b0b8bdb4b75b11942e663d97cbf5b6716f65e
samthanakorn <91516707+samthanakorn@users.noreply.github.com>
2021-10-03 19:55:27 +0700

    Update index_range_price.php

--- views/range_price/index_range_price.php
+++ views/range_price/index_range_price.php
@@ -10,12 +10,19 @@ search</button>
 
 
 
+
+
 <tr><td>id_range</td><td>min_range</td><td>max_range</td><td>price</td><td>color_price</td>
 <td>id_product</td><td>update</td><td>delete</td></tr>
 <?php foreach($range_price_List as $range_price)
 {
         echo "<tr><td>$range_price->id_range</td><td>$range_price->min_range</td><td>$range_price->max_range</td><td>$range_price->price</td>
-        <td>$range_price->color_price</td><td>$range_price->id_product</td><td>update</td><td>delete</td></tr>";
+        <td>$range_price->color_price</td><td>$range_price->id_product</td><td>
+        
+        <a href=?controller=range_price&action=updateForm&id_range=$range_price->id_range>update</a>
+        </td><td>
+        <a href=?controller=range_price&action=deleteConfirm&id_range=$range_price->id_range>delete</a>
+        </td></tr>";
 }
 echo "</table>";
 ?>
