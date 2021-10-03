b0481ae79b12912903c6f3e751f5b8bfa42dd336
bussakornPal <bussakorn.pal@ku.th>
2021-10-03 13:54:54 +0700

    update deleteConfirm

--- views/detail_order/deleteConfirm.php
+++ views/detail_order/deleteConfirm.php
@@ -1,6 +1,12 @@
 <?php echo "<br>Are you sure to delete this detail order<br>";
-    echo "รหัสรายละเอียด $detail_order->id_doc ";
-    echo "รหัสออร์เดอร์ $detail_order->id_order_cus<br>";?>
+    echo "รหัสรายละเอียด : $detail_order->id_doc ";
+    echo "รหัสออร์เดอร์ : $detail_order->id_order_cus<br>";
+    echo "สินค้า : $detail_order->name_product";
+    echo "จำนวนสีสกรีน : $detail_order->print_color";
+    echo "จำนวนที่สั่ง : $detail_order->amount_order_cus<br>";
+    
+
+?>
 <form method="get" action="">
     <input type="hidden" name="controller" value="detail_order"/>
     <input type="hidden" name="id" value="<?php echo $detail_order->id_doc;?>"/>
